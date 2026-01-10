b95e8bb27c3bf9ecbe317c798d8ccc3237a59961
shaikot07 <saifulislamshaikat007@gmail.com>
2026-01-10 08:53:35 +0600

    added the google font (popins)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,39 @@
+// import { Stack } from "expo-router";
+// import "./global.css";
+
+// export default function RootLayout() {
+//   return <Stack screenOptions={{ headerShown: false }} />;
+// }
+
+import {
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_600SemiBold,
+  Poppins_700Bold,
+} from "@expo-google-fonts/poppins";
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
-import "./global.css";
+import { Text } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import "./global.css"; // you can still keep this for Tailwind or other styles
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_600SemiBold,
+    Poppins_700Bold,
+  });
+
+  if (!fontsLoaded) return null;
+
+  //  TypeScript-safe way using `any`
+  (Text as any).defaultProps = (Text as any).defaultProps || {};
+  (Text as any).defaultProps.style = { fontFamily: "Poppins_400Regular" };
+
+  return (
+    <SafeAreaProvider>
+      <Stack screenOptions={{ headerShown: false }} />
+    </SafeAreaProvider>
+  );
 }
